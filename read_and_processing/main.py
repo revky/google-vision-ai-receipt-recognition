14104af8e3906a1e37ba8d8e84486010e40123d8
revky <filip.chwalinski@gmail.com>
2021-12-16 19:32:20 +0100

    Main setup, pre flask use

--- read_and_processing/main.py
+++ read_and_processing/main.py
@@ -1,9 +1,17 @@
 from process_text import ProcessText as pt
-from cloud_service import get_full_text_from_recipe
+from cloud_service import get_full_text_from_recipe, ApiConn
+from flask import Flask
+from flask_restful import Api, Resource
 
-def main(client, pic_path)->tuple[list]:
+app = Flask(__name__)
+api = Api(app)
+
+def main(client, pic_path)->list:
     full_text = get_full_text_from_recipe(pic_path, client)
     all_symbols = pt.get_list_of_symbols_from_full_text(full_text)
     rebuilded_lines = pt.build_custom_lines_of_text(all_symbols)
     corrected_lines = pt.correct_mistakes_in_rebuilded_lines(rebuilded_lines)
-    return rebuilded_lines, corrected_lines
+    return corrected_lines
+
+if __name__ == '__main__':
+    app.run(debug=True)
