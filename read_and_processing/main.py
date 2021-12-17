3361c4e95242942b4a9a76e857dc58a6d77be891
revky <filip.chwalinski@gmail.com>
2021-12-17 17:55:10 +0100

    Start of content exploration

--- read_and_processing/main.py
+++ read_and_processing/main.py
@@ -1,13 +1,6 @@
 from process_text import ProcessText as pt
 from cloud_service import get_full_text_from_recipe, ApiConn
-import argparse
-from pprint import pprint
 
-ap = argparse.ArgumentParser()
-ap.add_argument('-i','--image', required=True, help='path to image')
-ap.add_argument('-c','--cred', required=True, help='path to google api credentials')
-args = vars(ap.parse_args())
-api = ApiConn(args['cred'])
 
 def main(client, pic_path)->list:
     full_text = get_full_text_from_recipe(pic_path, client)
@@ -15,6 +8,3 @@ def main(client, pic_path)->list:
     rebuilded_lines = pt.build_custom_lines_of_text(all_symbols)
     corrected_lines = pt.correct_mistakes_in_rebuilded_lines(rebuilded_lines)
     return corrected_lines
-
-
-pprint(main(api.get_client(), args['image']))
